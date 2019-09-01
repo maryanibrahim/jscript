f1419a5223a3886cbbc655c8585744ee9212ed07
Maryan Ibrahim <maryanibrahim301@yahoo.com>
2019-08-31 22:09:43 -0400

    index108.js

---
+++ index108.js
@@ -0,0 +1,14 @@
+function sort (number1, number2, number3,) {
+
+    console.log(number2, number1, number3)
+
+    if(highToLow = true){
+        console.log(`hight to low: ${number3} ${number1} ${number2}`)
+    } else { 
+        console.log(`low to high: ${number2} ${number1} ${number3}`)
+    }
+
+    console.log (number1, number2, number3)
+}
+  
+  sort(10,8,25)
