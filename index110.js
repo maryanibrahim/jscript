4cc5c50c0ea27130fe6ab7a623c36c1d9296e91f
Maryan Ibrahim <maryanibrahim301@yahoo.com>
2019-08-31 22:13:14 -0400

    index110.js

---
+++ index110.js
@@ -0,0 +1,7 @@
+function average(number1, number2, number3, number4, number5,){
+
+    let average = (number1 + number2 + number3 + number4 + number5);
+    console.log(`The average is : ${average}`);
+      
+}
+average(2, 4, 10, 20, 30)
