7bf3205ee2e94b2083bf5c7bb40feeae29caec7f
Maryan Ibrahim <maryanibrahim301@yahoo.com>
2019-09-01 22:50:38 -0400

    index115.js

---
+++ index115.js
@@ -0,0 +1,7 @@
+const getlongerText = function(text1, text2) {
+    console.log(text1 + text2)
+}
+  
+  getlongerText('happy','day')
+  getlongerText('chocolate','icecream')
+  getlongerText('hello','canada')
