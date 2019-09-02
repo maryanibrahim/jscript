3d5112e8164e4e4427804376497a2673a10500f0
Maryan Ibrahim <maryanibrahim301@yahoo.com>
2019-09-01 22:52:08 -0400

    index116.js

---
+++ index116.js
@@ -0,0 +1,23 @@
+function getHexaColor(color) {
+    if( color === "white"){
+    console.log('hexa color is #FFFFFF')}
+    else if (color === "black"){ 
+    console.log('hexa color is #000000')}
+    else if (color === "blue"){ 
+    console.log('hexa color is #0b24fb')}
+    else if (color ==="green"){ 
+    console.log('hexa color is #0e7e12')}
+    else if (color === "yellow"){ 
+    console.log('hexa color is #fffd38')}
+    else if (color ==="pink"){ 
+    console.log('hexa color is #fec1cc')}
+    else
+         console.log('not a web color');
+  
+  }
+  getHexaColor('whiite');
+  getHexaColor('black');
+  getHexaColor('blue');
+  getHexaColor('green');
+  getHexaColor('yellow');
+  getHexaColor('pink')
