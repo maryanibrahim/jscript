14ff4a9309091a70798517e80e6f225493437d57
Maryan Ibrahim <maryanibrahim301@yahoo.com>
2019-09-02 23:01:58 -0400

    index123.js

---
+++ index123.js
@@ -0,0 +1,4 @@
+let  students = ['amal','mina','maryan','mark','kim','jonas'];
+
+console.log(`${students[1].toUpperCase()} & ${students[5].toUpperCase()}`);
+console.log (`The students array has "${students.length}" items`);
