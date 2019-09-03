6ebd7b9b1aa08de625720f3715f8ec984f276555
Maryan Ibrahim <maryanibrahim301@yahoo.com>
2019-09-02 23:03:49 -0400

    index132.js

---
+++ index132.js
@@ -0,0 +1,16 @@
+const mutants = ['Professor X','Cyclops','Iceman','Angel','Beast','Phoenix','Logan'];
+
+ 
+
+if (mutants.indexOf('logan') === -1 && mutants.indexOf('professor x') === -1)  {
+    console.log('We love X-Men')
+}
+
+
+if (mutants.indexOf('Gambit') > -1) {
+    console.log('we love x-men');
+  } else {
+    console.log('X-Men sucks');
+  }
+
+ 
