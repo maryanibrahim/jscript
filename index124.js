0e1c777779ece34a8fa8a0ae5eeae5f16cbb12f6
Maryan Ibrahim <maryanibrahim301@yahoo.com>
2019-09-02 23:02:56 -0400

    index124.js

---
+++ index124.js
@@ -0,0 +1,3 @@
+let  movies = [' Forrest Gump',' The Godfather',' The Green Mile','Goodfellas',' Scarface',' The Terminal',' Million Dollar Baby',' Millions',' As Good as It Gets ',' Babel'];
+console.log(movies[0],movies[9]);
+console.log(movies[ movies.length - 1 ]);
