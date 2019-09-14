ecd0588816b89a7387b6fe69b3e3fe3435d1dd79
Maryan Ibrahim <maryanibrahim301@yahoo.com>
2019-09-13 23:38:25 -0400

    index148.js

---
+++ index148.js
@@ -0,0 +1,12 @@
+let superhero = {
+    name:  'Batman',
+    secretName:  'Bruce',
+    sidekick:  ['Robin', 'Alfred', 'Gordon'],
+    strength:  70
+    };
+    
+    console.log(superhero);
+    
+    superhero.speed =  80;
+    
+    console.log(superhero);
