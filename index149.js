9ce39a8cc5d8138a3e719c9a3dbd79969e484bec
Maryan Ibrahim <maryanibrahim301@yahoo.com>
2019-09-13 23:52:28 -0400

    index149.js

---
+++ index149.js
@@ -0,0 +1,21 @@
+const person = {
+    name: "maryan",
+    age: 32,
+    isSingle: false,
+    location: {
+        city: "Winnipeg",
+        province: "Manitoba",
+    }
+
+  }
+
+  console.log(person, '\n')
+
+  person.isKind = true;
+  person.nationality = "Egypt";
+  person.volunteering = "webeditor";
+  person.lastName = "ibrahim";
+  person.height = 160;
+
+
+  console.log(person)
