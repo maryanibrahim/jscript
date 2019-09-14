a9791af13f9f2cbf4bd801fd90eb2e763fdeeeb0
Maryan Ibrahim <maryanibrahim301@yahoo.com>
2019-09-13 22:25:03 -0400

    index134.js

---
+++ index134.js
@@ -0,0 +1,37 @@
+const number=[];
+let total=0;
+for ( let i=0;i<=1000;i++)
+{
+  number.push(i);
+ 
+};
+const total = number.filter(function(number))
+{
+							return number+=number;
+							}
+console.log(number);
+
+
+
+
+
+/*
+const mutants = ['Professor X','Cyclops','Iceman','Angel','Beast','Phoenix','Logan','Gambit'];
+
+mutants.forEach(function(mutantsName) {
+    console.log(mutantsName);
+  });
+
+  console.log('-------------')
+
+  mutants.push('Magneto')
+
+  const Magneto = mutants.pop();
+
+ const mutantsInUpperCase = mutants.map(function(mutants) {
+    return mutants.toUpperCase();
+  });
+
+  console.log(mutantsInUpperCase);
+
+  */
